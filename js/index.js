718a1dbc4c20ee22d30b8832804535e78a61b195
gahhhenrikk <gah_oliveira1999@hotmail.com>
2018-02-17 00:40:56 -0200

    Adicionado Logica de proucurar usuario expecifico e, adicionar no input em cima da tabela.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,12 @@
+
 //console.log('indexjs carregado!');
 /*
  * Elementos do DOM 
 */
 let nome = document.querySelector('#nome_principal');
 let btnCarregarJson = document.querySelector('#Carregar');
-let erroAjax = document.querySelector('#erroAJAX');
 
-console.log(btnCarregarJson);
-console.log(erroAjax);
-
-
-
+//console.log(btnCarregarJson);
 
 /**
  * Logica de carregar JSON
@@ -19,21 +15,33 @@ let objeto_http = new XMLHttpRequest();
 
 btnCarregarJson.addEventListener('click', () => {
     //alert('boto clicado!');
+
     objeto_http.open("GET", "https://jsonplaceholder.typicode.com/users");
     objeto_http.addEventListener('load', () => {
         if (objeto_http.status == 200) {
             //console.log('conexao feita');
             ///console.log(objeto_http.responseText);
             //console.log(typeof `Tipo da resposta antes: ${objeto_http.responseText}`);
-            let usuarios = JSON.parse(objeto_http.responseText);
 
+            //let avisoSucesso = document.querySelector('#avisoS');
+            //avisoSucesso.classList.remove('invisivel');
+            let usuarios = JSON.parse(objeto_http.responseText);
+            let proucuraUsuario = document.querySelector('#email-query');
+            //console.log(proucuraUsuario);
             usuarios.forEach(usuario => {
+                if (usuario.username == 'Samantha') {
+                    console.log(usuario);
+                    proucuraUsuario.value = usuario.email;
+                }
                 adicionarUsuarionaTabela(usuario);
             });
-
         } else {
             //console.log(objeto_http.status == 400);
+            let erroAjax = document.querySelector('#erroAJAX');
+            let paiErroAjax = document.querySelector('.invisivel');
+            paiErroAjax.classList.remove('invisivel');
             erroAjax.textContent = `Opa, parece que algo deu errado ao tentar carregar os usuarios, tente novamente mais tarde!`;
+
         }
     });
     objeto_http.send();
@@ -45,18 +53,18 @@ btnCarregarJson.addEventListener('click', () => {
  */
 
 function adicionarUsuarionaTabela(usuario) {
-    console.log(usuario);
+    //console.log(usuario);
     let usuarioTr = montaTr(usuario);
     let tabela = document.querySelector('#tabela-usuarios');
-    console.log(tabela);
+    //console.log(tabela);
     tabela.appendChild(usuarioTr);
 }
 
 function montaTr(usuarios) {
     let usuarioTr = document.createElement("tr");
     usuarioTr.appendChild(montarTd(usuarios.id));
-    usuarioTr.appendChild(montarTd(usuarios.email));
-    usuarioTr.appendChild(montarTd(usuarios.name));
+    usuarioTr.appendChild(montarTd(usuarios.website));
+    //usuarioTr.appendChild(montarTd(usuarios.name));
 
     return usuarioTr;
 }
